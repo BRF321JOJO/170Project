06174aba40639d55e385fdaba447eb2309a28b2f
Brandon Fajardo <brandonrf01@gmail.com>
2021-04-14 23:20:33 -0700

    Initial graph creation file

---
+++ createGraph.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+V = 30
+G.add_nodes_from([0, V-1])
+
+e = (0, 1)
+#G.add_edge(*e)
+
+lollipop = nx.lollipop_graph(10, 20)
+lobster = nx.random_lobster(20, 0.7, 0.7)
+
+
+#nx.draw(G, with_labels=True, font_weight='bold')
+#nx.draw(lollipop, with_labels=True, font_weight='bold')
+nx.draw(lobster, with_labels=True, font_weight='bold')
+plt.show()
