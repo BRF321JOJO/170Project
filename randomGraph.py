f669a640196dfc5d62d6fb59b9d2a43955f886ca
Brandon Fajardo <brandonrf01@gmail.com>
2021-04-15 20:48:28 -0700

    Finished randomized graph creation

--- randomGraph.py
+++ randomGraph.py
@@ -3,33 +3,28 @@ import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-def main(numVertices) {
+def main(numVertices):
     G = nx.Graph()
+    G.add_nodes_from([0, numVertices-1]) #TODO: Ensure the 0 index
 
-    #TODO: Ensure the 0 index
-    G.add_nodes_from([0, numVertices-1])
+    for vertex in range(numVertices):
+        #List of vertices to connect to VERTEX
+        takenVertices = [i for i in range(numVertices)]
+        random.shuffle(takenVertices)
+        #Remove self-loop
+        takenVertices.remove(vertex)
 
-    for (vertex in range(numVertices)) {
-
-        takenVerticies = {}
-
-        for (edge in numVertices/2) {
-            #TODO: Add edge weight to this
-            randomVertex = random.randInt(numVertices)
-            while (randomVertex) {
-                
-            }
-            e = (vertex, randomVertex)
-            G.add_edge(*e)
-        }
-    }   
+        for edge in range(numVertices//2):
+            G.add_edge(vertex, takenVertices[edge], weight=round(random.uniform(0.001, 99.999), 3))
 
     nx.draw(G, with_labels=True, font_weight='bold')
-}
+    plt.show()
 
+    #Call parse on G
+    #Return output
 
+    #In command, pipe output to file
 
 
 if __name__ == "__main__":
-    main(sys.argv[0])
+    main(int(sys.argv[1]))
