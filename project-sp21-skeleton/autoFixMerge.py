dc40e2187939754cfca020b62f37b3d250eb1f80
BRF321JOJO <brandonrf01@gmail.com>
2021-04-30 16:17:40 -0700

    Created auto-merging files within mergeFolder

--- project-sp21-skeleton/autoFixMerge.py
+++ project-sp21-skeleton/autoFixMerge.py
@@ -11,21 +11,32 @@ if __name__ == '__main__':
 
     files = glob.glob('mergeFolder/*')
     files.sort()
+
+    improvements = 0
+
     for new_output in files:  # Iterates through every file to merge
+        print("Begin processing {}".format(new_output))
         input_path = size + "/" + new_output[12:]
         input_path = input_path[:len(input_path)-4] + ".in"
         input_path = "inputs/" + input_path
 
-        old_outputs = 'outputs/' + input_path[7:][:-3] + '.out'
+        old_output = 'outputs/' + input_path[7:][:-3] + '.out'
 
         G = read_input_file(input_path)
 
-        currBest_distance = read_output_file(G, old_outputs)
+        currBest_distance = read_output_file(G, old_output)
         this_distance = read_output_file(G, new_output)
 
-        if currBest_distance >= this_distance:
-            #print("Output distance IMPROVED by: " + str(this_distance - currBest_distance))
-            #print("NEW shortest path is length: " + str(this_distance))
-            print(new_output + " not improved")
+        if currBest_distance < this_distance:
+            improvements += 1
 
+            print("Output distance IMPROVED by: " + str(this_distance - currBest_distance))
+            currBest_file = open(old_output, "w")
+            merge_file = open(new_output, "r")
 
+            currBest_file.write(merge_file.read())
+
+            currBest_file.close()
+            merge_file.close()
+
+    print("Merge finished! " + str(improvements) + " files improved.")
